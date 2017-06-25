f6f2dfa03e2ddb0c5947659090f63b2b799fe959
AdamJames <satom99@users.noreply.github.com>
2017-06-25 13:42:12 +0200

    New class version

--- class.lua
+++ class.lua
@@ -5,41 +5,34 @@
 ]]
 
 local function recursive (object, class, ...)
-	local methods_meta = getmetatable(class)
-	if not methods_meta then return end
-	local methods = methods_meta.__index
-	local parent = getmetatable(methods).__index
-	recursive(object, parent, ...)
-	if methods.__constructor ~= parent.__constructor then
-		methods.__constructor(object, ...)
+	local parent = getmetatable(class)
+	parent = (parent and parent.__index)
+	if parent then
+		recursive(object, parent, ...)
+	end
+	local constructor = rawget(class, '__constructor')
+	if constructor then
+		constructor(object, ...)
 	end
 end
 
 return function(parent)
 	parent = parent or {}
-	local methods = setmetatable(
+	return setmetatable(
 		{},
 		{
 			__index = parent,
-		}
-	)
-	local class = setmetatable(
-		{},
-		{
-			__index = methods,
-			__newindex = methods,
 			__call = function(self, ...)
 				local object = setmetatable(
 					{},
 					{
-						__index = methods,
+						__index = self,
 					}
 				)
 				recursive(object, self, ...)
-				object.__constructor = nil
+				object.__constructor = false
 				return object
 			end,
 		}
 	)
-	return class
 end
